59de468b1284c9b3b1508ae7706819a81911d8b5
Andżelika Jezierska <a.b.jezierska@gmail.com>
2018-07-17 23:58:12 +0200

    speed up and ai

--- minipong.js
+++ minipong.js
@@ -12,40 +12,29 @@ const ballSize = 20;
 let ballX = cw / 2 - ballSize / 2;
 let ballY = ch / 2 - ballSize / 2;
 
-// polozenie pilki od 490 do 510px, uzylismy let bo te wartosci beda sie zmieniac w trakcie gry
-
 const paddelHeight = 100;
 const paddelWidth = 20;
 
 const playerX = 70;
 const aiX = 910;
-
-//początkowy X paletek, playerX - nasza, aiX - komputera
-
 let playerY = 200;
 let aiY = 200;
-
-//początkowy Y paletek, będzie się zmienial dlatego let
 
 const lineWidth = 6;
 const lineHeight = 12;
 
-let ballSpeedX = 1;
-let ballSpeedY = 1;
+let ballSpeedX = 2;
+let ballSpeedY = 2;
 
 function player() {
   ctx.fillStyle = 'yellow';
   ctx.fillRect(playerX, playerY, paddelWidth, paddelHeight);
 };
 
-//aktualna pozycja naszej rakietki
-
 function ai() {
   ctx.fillStyle = 'red';
   ctx.fillRect(aiX, aiY, paddelWidth, paddelHeight);
 };
-
-//aktualna pozycja rakietki
 
 function ball() {
   ctx.fillStyle = '#fff';
@@ -53,9 +42,17 @@ function ball() {
 
   ballX += ballSpeedX;
   ballY += ballSpeedY;
-};
 
-//aktualna pozycja pilki
+  if (ballY <= 0 || ballY + ballSize >= ch) {
+    ballSpeedY = -ballSpeedY;
+    speedUp();
+  };
+
+  if (ballX <= 0 || ballX + ballSize >= cw) {
+    ballSpeedX = -ballSpeedX;
+    speedUp();
+  };
+};
 
 function table() {
   ctx.fillStyle = 'black';
@@ -67,20 +64,89 @@ function table() {
   };
 };
 
-//rysowanie stolu
+topCanvas = canv.offsetTop;
+
+//odleglosc od poczatku okna przegladarki
+
+console.log(topCanvas);
+
+function playerPosition(e) {
+  playerY = e.clientY - topCanvas - paddelHeight / 2;
+
+  //gdy rakietka wyjeżdza na dole poza cavas
+
+  if (playerY >= ch - paddelHeight) {
+    playerY = ch - paddelHeight;
+  };
+
+  //gdy próbuje wyjachać rakietka na górze poza canvas
+
+  if (playerY <= 0) {
+    playerY = 0;
+  };
+
+  // aiY = playerY;
+};
+
+canv.addEventListener('mousemove', playerPosition);
+
+function speedUp() {
+  if (ballSpeedX > 0 && ballSpeedX < 16) {
+    ballSpeedX += 1;
+  }else if (ballSpeedX < 0 && ballSpeedX > -16) {
+    ballSpeedX -= 1;
+  }
+
+  if (ballSpeedY > 0 && ballSpeedY < 16) {
+    ballSpeedY += 1;
+  }else if (ballSpeedY < 0 && ballSpeedY > -16) {
+    ballSpeedY -= 1;
+  }
+}
+
+function aiPosition() {
+  const middlePaddel = aiY + paddelHeight / 2;
+  const middleBall = ballY + ballSize / 2;
+
+  if (ballX > 500) {
+    if (middlePaddel - middleBall > 200) {
+      aiY -= 24;
+    } else if (middlePaddel - middleBall > 50) {
+      aiY -= 10;
+    } else if (middlePaddel - middleBall < -200) {
+      aiY += 24;
+    } else if (middlePaddel - middleBall < -50) {
+      aiY += 10;
+    }
+  };
+
+  if (ballX <= 500 && ballX > 100) {
+    if (middlePaddel - middleBall > 100) {
+      aiY -= 3;
+    }
+
+    if (middlePaddel - middleBall < -100) {
+      aiY += 3;
+    }
+  }
+
+  if (aiY >= ch - paddelHeight) {
+    aiY = ch - paddelHeight;
+  }
+
+  if (aiY <= 0) {
+    aiY = 0;
+  }
+}
 
 function game() {
   table();
   ball();
   player();
   ai();
+  aiPosition();
 };
 
-// setInterval(game, 25);
+setInterval(game, 25);
 
-//funkcja ma byc wywolywana co 50 milisekund (1 sekunda = 1000 milisekund,
-// czyli przy 50, wywola sie 20 razy na sekunde)
-
-setInterval(game, 1000 / 60);
-
-//60 klatek na sekunde
+// setInterval ( game, 1000 / 60 );
